6fcf9362af171b15b2caae861fa32010262e88e1
Alexandro Arauco Sanchez <xander.as90@gmail.com>
2025-06-02 22:35:01 +0200

    Enhance QueryProvider with state management and loading fallback; update layout styles

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { QueryClient } from "@tanstack/react-query";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { useEffect, useState } from "react";
 
 interface QueryProviderProps {
   children: React.ReactNode;
 }
 
 export default function QueryProvider({ children }: QueryProviderProps) {
+  const [persistorReady, setPersistorReady] = useState(false);
+  const [persister, setPersister] = useState<any>(null);
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -15,9 +21,15 @@ export default function QueryProvider({ children }: QueryProviderProps) {
     },
   });
 
-  const persister = createSyncStoragePersister({
-    storage: window.localStorage,
-  });
+  useEffect(() => {
+    const storagePersister = createSyncStoragePersister({
+      storage: window.localStorage,
+    });
+    setPersister(storagePersister);
+    setPersistorReady(true);
+  }, []);
+
+  if (!persistorReady) return null; // or a fallback like <Loading />
 
   return (
     <PersistQueryClientProvider
