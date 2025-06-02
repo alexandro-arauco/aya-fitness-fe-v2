0ede7490db2692b3e53f16ea42e1f24d80c0ddad
Alexandro Arauco Sanchez <xander.as90@gmail.com>
2025-06-02 22:29:04 +0200

    Add QueryProvider component and integrate it into the layout

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,30 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      },
+    },
+  });
+
+  const persister = createSyncStoragePersister({
+    storage: window.localStorage,
+  });
+
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
+  );
+}
